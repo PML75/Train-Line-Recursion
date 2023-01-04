49e98ebb5077a296d627cd2fd299cba02f9aea90
Phu Lam <43253532+PML75@users.noreply.github.com>
2023-01-04 13:20:28 -0800

    Create train.h

---
+++ train.h
@@ -0,0 +1,31 @@
+#include <memory>
+#include <string>
+
+class Train {
+ public:
+  Train(int passenger_count, int seat_count,
+        std::shared_ptr<Train> next_carriage)
+      : passenger_count_(passenger_count),
+        seat_count_(seat_count),
+        next_carriage_(next_carriage) {}
+
+  int GetPassengerCount() const {
+    return passenger_count_;
+  }
+  int GetSeatCount() const {
+    return seat_count_;
+  }
+  std::shared_ptr<Train> GetNextCarriage() const {
+    return next_carriage_;
+  }
+
+  int TotalTrainPassengers();
+  int MaxCarriagePassengers();
+  bool IsTrainFull();
+  void AddCarriageToEnd(std::shared_ptr<Train> nCarriage);
+
+ private:
+  int passenger_count_;
+  int seat_count_;
+  std::shared_ptr<Train> next_carriage_;
+};
